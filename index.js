3b9e96dfd23dd8380ab5792e0554eb375fcc0004
Jonathan Corley <jcorley@noscihigh.org>
2021-01-29 17:03:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,156 @@
+///////////////////////////
+///// Unit 0 Practice /////
+///////////////////////////
+
+
+/* 1.
+Write a greeter function that will display 
+'Hello World' to the console.
+*/
+
+
+
+
+/* 2.
+Write a function that will calculate the sum of two 
+numbers.
+
+Example:
+divide(3, 9) => 12
+divide(9, 9) => 18
+
+*/
+
+
+
+
+/* 3.
+Write a function that will calculate the difference
+of two numbers.
+
+Example:
+divide(3, 9) => -6
+divide(9, 9) => 0
+
+*/
+
+
+
+
+/* 4.
+Write a function that will calculate the quotient
+of two numbers.
+
+Example:
+divide(3, 9) => 0.3333333
+divide(9, 9) => 1
+
+*/
+
+
+
+
+
+/* 5.
+Write a function that will calculate the product
+of two numbers.
+
+Example:
+multiply(3, 9) => 27
+multiply(9, 9) => 81
+
+*/
+
+
+
+
+
+
+/* 6.
+Write a function that will return years into days.
+
+Example:
+yearsToDays(3) => 1095
+yearsToDays(9) => 3285
+
+*/
+
+
+
+
+/* 7.
+Write a function that will return the first element
+in a given array.
+
+
+Example:
+firstElement([0,1,2,3]) => 0
+yearsToDays(['hello', 'world']) => 'hello'
+
+*/
+
+
+
+
+
+
+/* 7.
+Write a function that will return the first element
+in a given array.
+
+
+Example:
+firstElement([0,1,2,3]) => 0
+yearsToDays(['hello', 'world']) => 'hello'
+
+*/
+
+
+
+
+
+
+/* 8.
+Write a function that will convert minutes into 
+seconds. 
+
+
+Example:
+minToSec(60) => 3600
+minToSec(1) => 60
+
+*/
+
+
+
+
+
+
+
+/* 9.
+Write a function that will convert seconds into 
+hours. 
+
+
+Example:
+secToHours(3600) => 1
+secToHours(1800) => 0.5
+
+*/
+
+
+
+
+
+
+
+/* 10.
+Write a function that will return the remainder of 
+the division of two numbers. 
+
+
+Example:
+remainder(5, 2) => 1
+remainder(10, 7) => 0.5
+
+*/
